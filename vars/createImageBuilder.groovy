a2bf860e7ca1a130f0dfc23b7a966f53fda75f82
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2019-12-11 16:52:43 +0100

    changed path to sourcepole gitlab

--- vars/createImageBuilder.groovy
+++ vars/createImageBuilder.groovy
@@ -3,7 +3,7 @@ def call(List credentials, String args){
         openshift.withProject('gdi-devel') {
             if ( !openshift.selector("bc" , "qwc-service").exists() )
                                    // build qwc-service Image from Sourcepole Source Code
-                openshift.newBuild( "https://git.sourcepole.ch/ktso/somap", "${args}")
+                openshift.newBuild( "https://git.sourcepole.ch/ktso/somap.git", "${args}")
                 withCredentials(credentials) {
                 openshift.verbose()
                 //openshift.newBuild( "https://${githubUser}:${githubPwd}@github.com/pfeimich/qwc-service.git", "${args}")
