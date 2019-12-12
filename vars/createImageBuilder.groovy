5c09bb6c7fdd1cb8ad52b5b57c0ed13b55a72142
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2019-12-12 14:36:48 +0100

    changed repo connection back to user:password

--- vars/createImageBuilder.groovy
+++ vars/createImageBuilder.groovy
@@ -2,11 +2,9 @@ def call(List credentials, String args){
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
             if ( !openshift.selector("bc" , "qwc-service").exists() )
-                                   // build qwc-service Image from Sourcepole Source Code
-                openshift.newBuild( "https://git.sourcepole.ch/ktso/somap.git", "${args}")
                 withCredentials(credentials) {
                 openshift.verbose()
-                //openshift.newBuild( "https://${githubUser}:${githubPwd}@github.com/pfeimich/qwc-service.git", "${args}")
+                openshift.newBuild( "https://${gitlabUser}:${gitlabPwd}@git.sourcepole.ch/ktso/somap.git", "${args}")
                 openshift.verbose(false)
             }
         }
