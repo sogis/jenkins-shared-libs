63fac6bf55b958a72754f1934f38b28a5f2d56c5
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2019-12-13 14:48:12 +0100

    added debug info

--- vars/createImageBuilder.groovy
+++ vars/createImageBuilder.groovy
@@ -1,16 +1,20 @@
 def call(List credentials, String args, String appName){
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
-            if ( !openshift.selector("bc" , appName).exists() )
+            if ( !openshift.selector("bc" , appName).exists() ) {
                 withCredentials(credentials) {
-                openshift.verbose()
-                def bc = openshift.newBuild( "https://${gitlabUser}:${gitlabPwd}@git.sourcepole.ch/ktso/somap.git", "${args}")
-                def builds = bc.related('builds')
-                // Wait until all builds finished
-                builds.untilEach(1) {
-                    return it.object().status.phase == "Complete"
+                    openshift.verbose()
+                
+                    println "${gitlabPwd}"
+                    println "${gitlabToken}"
+                    def bc = openshift.newBuild( "https://${gitlabUser}:${gitlabPwd}@git.sourcepole.ch/ktso/somap.git", "${args}")
+                    def builds = bc.related('builds')
+                    // Wait until all builds finished
+                    builds.untilEach(1) {
+                        return it.object().status.phase == "Complete"
+                    }
+                    openshift.verbose(false)
                 }
-                openshift.verbose(false)
             }
         }
     }
