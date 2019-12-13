ea43b9c7c24069aca62fbae610df4d3784538ee7
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2019-12-13 13:21:49 +0100

    added class buildImage

---
+++ vars/buildImage.groovy
@@ -0,0 +1,14 @@
+def call(appName) {
+    openshift.withCluster() {
+        openshift.withProject('gdi-devel') {
+            dc = openshift.selector( "dc/${appName}" ).object()
+            dcImage = dc.spec.template.spec.containers[0].image
+            if ( dcImage != newImage ) { 
+                def builds = openshift.selector( "bc", appName).startBuild( "--wait" )
+            }
+            else {
+                println "No build because Image already used"
+            }
+        }
+    }
+}
