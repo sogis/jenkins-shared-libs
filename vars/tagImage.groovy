02686e3543b0b4a25785eaba9e58ee2ef3bd8ae4
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-01-15 13:16:49 +0100

    fixed bug when dc not yet exists

--- vars/tagImage.groovy
+++ vars/tagImage.groovy
@@ -1,6 +1,13 @@
 def call(String appName, String version) {
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
+            if ( openshift.selector( "dc" , ${appName} ).exists() {
+                dc = openshift.selector( "dc/${appName}" ).object()
+                dcImage = dc.spec.template.spec.containers[0].image
+            }
+            else {
+                dcImage = ''
+            }
             if ( dcImage != newImage ) { 
                 openshift.tag( "${appName}:latest", "${appName}:${version}")
             }
