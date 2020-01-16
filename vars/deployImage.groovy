ea174e9b8b82013083d962f8fec49e44a7892e49
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-01-16 13:15:18 +0100

    refactored deployImage

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,23 +1,10 @@
-def call(String appName, String version, String repo, String stage) {
+def call(String appName, String repo, String stage) {
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
-            def dc = openshift.process( "${repo}/${appName}.yaml", "-p", "NAMESPACE=${stage}" )
-            if ( openshift.selector( "dc/${appName}" ).exists() ) {
-                dc = openshift.selector( "dc/${appName}" ).object()
-                dcImage = dc.spec.template.spec.containers[0].image
+            def dcConfig = openshift.process( "${repo}/deploymentconfig.yaml", "-p", "NAMESPACE=${stage}" )
+            openshift.apply( dcConfig )
+            dc = openshift.selector( "dc/${appName}" )
+            dc.rollout().status()
             }
-            else {
-                dcImage = ''
-            }
-            if ( dcImage != version ) { 
-                for ( o in dc ) {
-                    o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/gdi-devel/${appName}:${version}"
-                }
-            }
-            openshift.apply( dc )
-            else {
-                println "No new tag for ${appName} Image because tag already exists"
-            }
-        }
+        }  
     }
-}
