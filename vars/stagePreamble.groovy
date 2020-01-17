b817697f63e1880f364c9429b0f65abbb73a75e3
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-01-17 08:47:17 +0100

    Test for user Input

--- vars/stagePreamble.groovy
+++ vars/stagePreamble.groovy
@@ -1,15 +1,31 @@
 def call(){
     def version
-    def userInput = input(
-        id: 'userInput', message: 'Enter version number',
+    def selectVersion = input(
+        id: 'selectVersion', message: 'Enter version number',
         parameters: [
             string(defaultValue: 'None',
                    description: 'Version number',
                    name: 'Version')
         ])
 
-    version = userInput?:''
+    version = selectVersion?:''
     echo "${version}"
+    //def buildImage = input(
+    //    id: 'buildImage', message: 'Build Image?',
+    //    parameters: [
+    //        boolean(defaultValue: 'None',
+    //               description: 'Auswahl ob ein Image gebaut werden soll oder nicht',
+    //               name: 'Build')
+    //    ])
+    def INPUT_PARAMS = input message: 'Please Provide Parameters', ok: 'Next',
+                                        parameters: [
+                                        choice(name: 'ENVIRONMENT', choices: ['dev','qa'].join('\n'), description: 'Please select the Environment'),
+                                        choice(name: 'IMAGE_TAG', choices: getDockerImages(), description: 'Available Docker Images')]
+                        env.ENVIRONMENT = INPUT_PARAMS.ENVIRONMENT
+                        env.IMAGE_TAG = INPUT_PARAMS.IMAGE_TAG
+                    }
+
+    version = userInput?:''
     newImage = "docker-registry.default.svc:5000/gdi-devel/qwc-service:${version}"
     
     openshift.withCluster() {
