2392eb599f9747a0efb053f6fd73b84d6d3d6b36
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-01-17 09:09:00 +0100

    adapted input example for own use

--- vars/stagePreamble.groovy
+++ vars/stagePreamble.groovy
@@ -1,36 +1,26 @@
 def call(){
     def version
-    def selectVersion = input(
-        id: 'selectVersion', message: 'Enter version number',
+    def building
+    def inputParams = input(
+        id: 'selectVersion', message: 'Bitte Version angeben und ob das Image gebaut werden soll',
         parameters: [
             string(defaultValue: 'None',
-                   description: 'Version number',
-                   name: 'Version')
+                   description: 'Bitte Version auswählen',
+                   name: 'version'),
+            choice(defaultValue: 'ja', 
+                   choices: ['ja','nein'].join('\n'),
+                   description: 'Soll das Image gebaut werden?'
+                   name: 'build'
         ])
 
-    version = selectVersion?:''
+    version = inputParams.version
+    building = inputParams.build
     echo "${version}"
-    //def buildImage = input(
-    //    id: 'buildImage', message: 'Build Image?',
-    //    parameters: [
-    //        boolean(defaultValue: 'None',
-    //               description: 'Auswahl ob ein Image gebaut werden soll oder nicht',
-    //               name: 'Build')
-    //    ])
-    def INPUT_PARAMS = input message: 'Please Provide Parameters', ok: 'Next',
-                                        parameters: [
-                                        choice(name: 'ENVIRONMENT', choices: ['dev','qa'].join('\n'), description: 'Please select the Environment'),
-                                        choice(name: 'IMAGE_TAG', choices: ['1','2'].join('\n'), description: 'Available Docker Images')]
-    env.ENVIRONMENT = INPUT_PARAMS.ENVIRONMENT
-    env.IMAGE_TAG = INPUT_PARAMS.IMAGE_TAG
-
-    version = userInput?:''
-    newImage = "docker-registry.default.svc:5000/gdi-devel/qwc-service:${version}"
     
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
             echo "Using project: ${openshift.project()}"
         }
     }
-    return version
+    return version, building
 }
