f6a78dc3f6ca0934ae9defc3daf54e48e6eeabe3
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-01-17 15:33:13 +0100

    Added some debug infos

--- vars/stagePreamble.groovy
+++ vars/stagePreamble.groovy
@@ -2,7 +2,9 @@ def call(){
     def version
     def building
     def inputParams = input(
-        id: 'selectVersion', message: 'Bitte Version angeben und ob das Image gebaut werden soll',
+        id: 'selectVersion', 
+        message: 'Bitte Version angeben und ob das Image gebaut werden soll',
+        ok: 'Start Pipeline'
         parameters: [
             string(defaultValue: 'None',
                    description: 'Bitte Version auswählen',
@@ -17,5 +19,7 @@ def call(){
             echo "Using project: ${openshift.project()}"
         }
     }
+    println inputParams.version
+    println inputParams.build
     return inputParams
 }
