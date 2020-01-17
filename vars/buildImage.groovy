384a4cf7b2ac54c747f68c606e60fcd81974e324
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-01-17 14:42:55 +0100

    refactored buildImage

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,23 +1,9 @@
-def call(String appName, String newImage, String repo) {
+def call(String appName, String newImage, String repo, String build) {
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
-            if ( openshift.selector( "dc/${appName}" ).exists() ) {
-                dc = openshift.selector( "dc/${appName}" ).object()
-                dcImage = dc.spec.template.spec.containers[0].image
-            }
-            else {
-                dcImage = ''
-            }
-            if ( dcImage != newImage ) { 
-                println dcImage
-                println newImage
-                def buildConfig = openshift.process( "${repo}/buildconfig.yaml" )
-                openshift.apply( buildConfig )
-                def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${newImage}", "--build-arg GIT_VERSION=${newImage}" )
-            }
-            else {
-                println "No build for ${appName} because Image already used"
+            def buildConfig = openshift.process( "${repo}/buildconfig.yaml" )
+            openshift.apply( buildConfig )
+            def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${newImage}", "--build-arg GIT_VERSION=${newImage}" )
             }
         }
     }
-}
