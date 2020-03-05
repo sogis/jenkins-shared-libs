267d0afefc7df32ad10546dd4c992ab115690cd7
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-03-05 18:13:47 +0100

    added v to GIT_VERSION param

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,7 @@ def call(String appName, String newImage, String repo, String build) {
         openshift.withProject('gdi-devel') {
             def buildConfig = openshift.process( "${repo}/buildconfig.yaml" )
             openshift.apply( buildConfig )
-            def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${newImage}", "--build-arg GIT_VERSION=${newImage}" )
+            def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${newImage}", "--build-arg GIT_VERSION=v${newImage}" )
             }
         }
     }
