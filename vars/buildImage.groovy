c941b224438746e499ea987ae8bcad5936ef3493
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-03-06 15:47:06 +0100

    changed variables

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,12 @@
-def call(String appName, String version, String qwcVersion, String repo, String build, String gitlabToken, String gitlabPwd) {
+def call(String appName, String version, String qwcVersion, String repo, String build, List credentials) {
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
             def buildConfig = openshift.process( "${repo}/buildconfig.yaml" )
             openshift.apply( buildConfig )
-            println gitlabToken
-            def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "GIT_PASSWORD=${gitlabPwd}" )
+            withCredentials(credentials) {
+                println ${gitlabToken}
+                def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "GIT_PASSWORD=${gitlabPwd}" )
+                }  
             }
         }
     }
