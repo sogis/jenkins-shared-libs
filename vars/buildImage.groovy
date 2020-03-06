b3273b8e6e1bf2a076973d4123074351e71ed4e1
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-03-06 15:56:22 +0100

    added missing env

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,7 +4,7 @@ def call(String appName, String version, String qwcVersion, String repo, String 
             def buildConfig = openshift.process( "${repo}/buildconfig.yaml" )
             openshift.apply( buildConfig )
             withCredentials(credentials) {
-                def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "GIT_PASSWORD=${gitlabPwd}" )
+                def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "--env GIT_PASSWORD=${gitlabPwd}" )
                 }  
             }
         }
