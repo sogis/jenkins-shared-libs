4dc895b637d66881dc253d4c5767d3ba6d60fa7f
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-03-06 15:18:40 +0100

    added debug info

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,6 +3,7 @@ def call(String appName, String version, String qwcVersion, String repo, String 
         openshift.withProject('gdi-devel') {
             def buildConfig = openshift.process( "${repo}/buildconfig.yaml" )
             openshift.apply( buildConfig )
+            println gitlabToken
             def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "GIT_PASSWORD=${gitlabPwd}" )
             }
         }
