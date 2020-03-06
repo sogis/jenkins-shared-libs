06aa610bc4ab5a6da1f4fec6e198036c84e5f047
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-03-06 15:05:58 +0100

    removed debug info and added new env variables for startbuild

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,9 @@
-def call(String appName, String version, String qwcVersion, String repo, String build) {
+def call(String appName, String version, String qwcVersion, String repo, String build, String gitlabToken, String gitlabPwd) {
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
             def buildConfig = openshift.process( "${repo}/buildconfig.yaml" )
             openshift.apply( buildConfig )
-            println qwcVersion
-            def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}" )
+            def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "GIT_PASSWORD=${gitlabPwd}" )
             }
         }
     }
