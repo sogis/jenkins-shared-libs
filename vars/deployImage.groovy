8cea570c6056b850124ff30627a91e453cba0b9c
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-03-13 08:11:18 +0100

    added setting version of image in dc

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,7 +1,10 @@
-def call(String appName, String repo, String stage) {
+def call(String appName, String repo, String stage, String version) {
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
             def dcConfig = openshift.process( "${repo}/deploymentconfig.yaml", "-p", "NAMESPACE=${stage}" )
+            for ( o in dc ) {
+                o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/gdi-devel/qwc-service:${version}"
+                }
             openshift.apply( dcConfig )
             dc = openshift.selector( "dc/${appName}" )
             dc.rollout().status()
