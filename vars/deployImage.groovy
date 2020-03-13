bb510b142e2743982927e017f951d609977d0df7
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-03-13 08:18:45 +0100

    fixed wrong parameter name

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -2,7 +2,7 @@ def call(String appName, String repo, String stage, String version) {
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
             def dcConfig = openshift.process( "${repo}/deploymentconfig.yaml", "-p", "NAMESPACE=${stage}" )
-            for ( o in dc ) {
+            for ( o in dcConfig ) {
                 o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/gdi-devel/qwc-service:${version}"
                 }
             openshift.apply( dcConfig )
