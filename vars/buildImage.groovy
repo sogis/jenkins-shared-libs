d6963f2ef0539390681b2a2fa5dce536df2336a4
pfeimich <mpfeiffer1975@gmail.com>
2020-03-18 14:54:56 +0100

    added empty string in if condition

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,7 +4,7 @@ def call(String appName, String version, String repo, String build, List credent
             def buildConfig = openshift.process( "${repo}/buildconfig.yaml" )
             openshift.apply( buildConfig )
             withCredentials(credentials) {
-                if (qwcVersion) {
+                if (qwcVersion != "") {
                     def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "--env GIT_PASSWORD=${gitlabPwd}" )
                     }
                 else {
