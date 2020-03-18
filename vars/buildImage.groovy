68a8d0843fb7b7c9d743977e04d0f1a4e1a1f6b5
pfeimich <mpfeiffer1975@gmail.com>
2020-03-18 14:27:59 +0100

    changed parameter order and added condition if qwcVersion does not exist

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,15 @@
-def call(String appName, String version, String qwcVersion, String repo, String build, List credentials) {
+def call(String appName, String version, String repo, String build, List credentials, String qwcVersion) {
     openshift.withCluster() {
         openshift.withProject('gdi-devel') {
             def buildConfig = openshift.process( "${repo}/buildconfig.yaml" )
             openshift.apply( buildConfig )
             withCredentials(credentials) {
-                def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "--env GIT_PASSWORD=${gitlabPwd}" )
+                if (qwcVersion) {
+                    def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "--env GIT_PASSWORD=${gitlabPwd}" )
+                    }
+                else {
+                    def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "--env GIT_PASSWORD=${gitlabPwd}" )
+                    }
                 }  
             }
         }
