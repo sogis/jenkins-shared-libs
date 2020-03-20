369a34f12f60f6be8caacd27f51a5e6789a0c703
pfeimich <mpfeiffer1975@gmail.com>
2020-03-20 11:41:55 +0100

    Add missing parameter

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -2,7 +2,7 @@ def call(List credentials, String repo, String stage){
     openshift.withCluster() {
         openshift.withProject( stage ) {
             withCredentials ( credentials ) { 
-                def ressources = openshift.process( "${repo}/ressources.yaml" , "-p", "DB_SERVER=geodb-t.rootso.org", "-p", "PW_OGC_SERVER=${PwdOgcServer}", "-p", "GEO_DB_SERVER=geodb-t.verw.rootso.org", "-p", "PW_SOGIS_SERVICE=${PwdSogisService}" )
+                def ressources = openshift.process( "${repo}/ressources.yaml" , "-p", "DB_SERVER=geodb-t.rootso.org", "-p", "PW_OGC_SERVER=${PwdOgcServer}", "-p", "GEO_DB_SERVER=geodb-t.verw.rootso.org", "-p", "PW_SOGIS_SERVICE=${PwdSogisService}", "-p", "NAMESPACE=${stage}" )
                 openshift.apply( ressources )
             }
         }
