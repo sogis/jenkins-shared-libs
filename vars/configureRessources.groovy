71e3a8e16a0e5e17eeb2dfa560de4e1b8d80aeab
pfeimich <mpfeiffer1975@gmail.com>
2020-03-23 16:44:59 +0100

    try all infos directly in Jenkinsfile

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -4,7 +4,7 @@ def call(List credentials, String repo, String stage, String parameters){
             withCredentials ( credentials ) { 
                 // def ressources = openshift.process( "${repo}/ressources.yaml" , "-p", "DB_SERVER=geodb-t.rootso.org", "-p", "PW_OGC_SERVER=${PwdOgcServer}", "-p", "GEO_DB_SERVER=geodb-t.verw.rootso.org", "-p", "PW_SOGIS_SERVICE=${PwdSogisService}", "-p", "NAMESPACE=${stage}" )
                 println parameters
-                def ressources = openshift.process( "${repo}/ressources.yaml", parameters)
+                def ressources = openshift.process( parameters)
                 openshift.apply( ressources )
             }
         }
