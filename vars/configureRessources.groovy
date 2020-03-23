06173def02476926ebc37356166a9dec1d3db839
pfeimich <mpfeiffer1975@gmail.com>
2020-03-23 14:15:28 +0100

    test process parameters in variable

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,8 +1,9 @@
-def call(List credentials, String repo, String stage){
+def call(List credentials, String repo, String stage, String parameters){
     openshift.withCluster() {
         openshift.withProject( stage ) {
             withCredentials ( credentials ) { 
-                def ressources = openshift.process( "${repo}/ressources.yaml" , "-p", "DB_SERVER=geodb-t.rootso.org", "-p", "PW_OGC_SERVER=${PwdOgcServer}", "-p", "GEO_DB_SERVER=geodb-t.verw.rootso.org", "-p", "PW_SOGIS_SERVICE=${PwdSogisService}", "-p", "NAMESPACE=${stage}" )
+                // def ressources = openshift.process( "${repo}/ressources.yaml" , "-p", "DB_SERVER=geodb-t.rootso.org", "-p", "PW_OGC_SERVER=${PwdOgcServer}", "-p", "GEO_DB_SERVER=geodb-t.verw.rootso.org", "-p", "PW_SOGIS_SERVICE=${PwdSogisService}", "-p", "NAMESPACE=${stage}" )
+                def ressources = openshift.process( "${repo}/ressources.yaml", parameters)
                 openshift.apply( ressources )
             }
         }
