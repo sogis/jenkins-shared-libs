24dc1230fd996efb6461e254676b8aa696af8d9d
pfeimich <mpfeiffer1975@gmail.com>
2020-03-23 22:41:59 +0100

    test with shell script

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,12 +1,15 @@
 def call(List credentials, String repo, String stage, String parameters){
-    openshift.withCluster() {
-        openshift.withProject( stage ) {
-            withCredentials ( credentials ) { 
+    sh """
+       oc process $parameters | oc apply -f- 
+    """
+    // openshift.withCluster() {
+        // openshift.withProject( stage ) {
+            // withCredentials ( credentials ) { 
                 // def ressources = openshift.process( "${repo}/ressources.yaml" , "-p", "DB_SERVER=geodb-t.rootso.org", "-p", "PW_OGC_SERVER=${PwdOgcServer}", "-p", "GEO_DB_SERVER=geodb-t.verw.rootso.org", "-p", "PW_SOGIS_SERVICE=${PwdSogisService}", "-p", "NAMESPACE=${stage}" )
-                println parameters
-                def ressources = openshift.process( parameters)
-                openshift.apply( ressources )
-            }
-        }
-    }
+                // println parameters
+                // def ressources = openshift.process( parameters)
+                // openshift.apply( ressources )
+       //     }
+      //  }
+   //  }
 }
