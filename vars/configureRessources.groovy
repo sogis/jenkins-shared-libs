63d28b0f29acaf51c90a6fe8006b9767ad70ad65
pfeimich <mpfeiffer1975@gmail.com>
2020-03-23 16:34:23 +0100

    added debug information

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -3,6 +3,7 @@ def call(List credentials, String repo, String stage, String parameters){
         openshift.withProject( stage ) {
             withCredentials ( credentials ) { 
                 // def ressources = openshift.process( "${repo}/ressources.yaml" , "-p", "DB_SERVER=geodb-t.rootso.org", "-p", "PW_OGC_SERVER=${PwdOgcServer}", "-p", "GEO_DB_SERVER=geodb-t.verw.rootso.org", "-p", "PW_SOGIS_SERVICE=${PwdSogisService}", "-p", "NAMESPACE=${stage}" )
+                println parameters
                 def ressources = openshift.process( "${repo}/ressources.yaml", parameters)
                 openshift.apply( ressources )
             }
