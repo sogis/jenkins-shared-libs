6e613a38b704ab7219e455a03ad59b0234a87fff
pfeimich <mpfeiffer1975@gmail.com>
2020-03-25 17:07:23 +0100

    added missing namespace parameter

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,7 +1,7 @@
 def call(String appName, String repo, String stage, String version, String parameters) {
     sh """
        oc process -f ${repo}/deploymentconfig.yaml $parameters | oc apply -n $stage -f-
-       oc patch dc $appName -p '{"spec":{"template":{"spec":{"containers":[{"name":"sogis-$appName","image":"docker-registry.default.svc:5000/$stage/$appName:$version"}]}}}}'
+       oc patch dc $appName -n ${stage} -p '{"spec":{"template":{"spec":{"containers":[{"name":"sogis-$appName","image":"docker-registry.default.svc:5000/$stage/$appName:$version"}]}}}}'
        oc rollout status
     """
    // openshift.withCluster() {
