ce478955368b90b7540cbbc5798da6c3017dcf25
pfeimich <mpfeiffer1975@gmail.com>
2020-03-25 22:39:36 +0100

    test without rollout

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -2,17 +2,6 @@ def call(String appName, String repo, String stage, String version, String param
     sh """
        oc process -f ${repo}/deploymentconfig.yaml $parameters | oc apply -n $stage -f-
        oc patch dc $appName -n ${stage} -p '{"spec":{"template":{"spec":{"containers":[{"name":"sogis-$appName","image":"docker-registry.default.svc:5000/$stage/$appName:$version"}]}}}}'
-       oc rollout -n $stage status dc $appName 
     """
-   // openshift.withCluster() {
-   //     openshift.withProject('gdi-devel') {
-   //         def dcConfig = openshift.process( "${repo}/deploymentconfig.yaml", "-p", "NAMESPACE=${stage}" )
-   //         for ( o in dcConfig ) {
-   //             o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/gdi-devel/qwc-service:${version}"
-   //             }
-   //         openshift.apply( dcConfig )
-   //         dc = openshift.selector( "dc/${appName}" )
-   //         dc.rollout().status()
-   //         }
-   //     }  
+    //   oc rollout -n $stage status dc $appName 
     }
