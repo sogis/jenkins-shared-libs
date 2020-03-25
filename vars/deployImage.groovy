b56402325eeda5ad95a2d4a547a13b45cb04683c
pfeimich <mpfeiffer1975@gmail.com>
2020-03-25 22:18:00 +0100

    added dc to rollout command

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -2,7 +2,7 @@ def call(String appName, String repo, String stage, String version, String param
     sh """
        oc process -f ${repo}/deploymentconfig.yaml $parameters | oc apply -n $stage -f-
        oc patch dc $appName -n ${stage} -p '{"spec":{"template":{"spec":{"containers":[{"name":"sogis-$appName","image":"docker-registry.default.svc:5000/$stage/$appName:$version"}]}}}}'
-       oc rollout status
+       oc rollout status dc $appName
     """
    // openshift.withCluster() {
    //     openshift.withProject('gdi-devel') {
