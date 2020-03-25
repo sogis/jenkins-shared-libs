7df562085298ff6cdcf2fa2dc011aa496cee677c
pfeimich <mpfeiffer1975@gmail.com>
2020-03-25 16:22:57 +0100

    changed deployImage to scripted pipeline

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,13 +1,18 @@
 def call(String appName, String repo, String stage, String version) {
-    openshift.withCluster() {
-        openshift.withProject('gdi-devel') {
-            def dcConfig = openshift.process( "${repo}/deploymentconfig.yaml", "-p", "NAMESPACE=${stage}" )
-            for ( o in dcConfig ) {
-                o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/gdi-devel/qwc-service:${version}"
-                }
-            openshift.apply( dcConfig )
-            dc = openshift.selector( "dc/${appName}" )
-            dc.rollout().status()
-            }
-        }  
-    }
+    sh """
+       oc process $parameters | oc apply -n $stage -f-
+       oc patch dc $appName -p '{"spec":{"template":{"spec":{"containers":[{"name":"sogis-$appName","image":"docker-registry.default.svc:5000/$stage/$appName:$version"}]}}}}'
+       oc rollout status
+    """
+   // openshift.withCluster() {
+   //     openshift.withProject('gdi-devel') {
+   //         def dcConfig = openshift.process( "${repo}/deploymentconfig.yaml", "-p", "NAMESPACE=${stage}" )
+   //         for ( o in dcConfig ) {
+   //             o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/gdi-devel/qwc-service:${version}"
+   //             }
+   //         openshift.apply( dcConfig )
+   //         dc = openshift.selector( "dc/${appName}" )
+   //         dc.rollout().status()
+   //         }
+   //     }  
+   // }
