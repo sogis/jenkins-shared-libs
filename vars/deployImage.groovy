e2c985edb701b3372ecd94842062c431192d7c35
pfeimich <mpfeiffer1975@gmail.com>
2020-03-26 13:41:08 +0100

    fixed bugs

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,11 +1,15 @@
 def call(String appName, String repo, String stage, String version, List dcConfig, String parameters) {
-    println dcConfig
-    for  ( o in dcConfig ) {
-        o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/gdi-devel/qwc-service:${version}"
+    openshift.withCluster() {
+        openshift.withProject(stage) {
+            println dcConfig
+            for  ( o in dcConfig ) {
+                o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/gdi-devel/qwc-service:${version}"
+                }
+            openshift.apply( dcConfig )
+            dc = openshift.selector( "dc/${appName}" )
+            dc.rollout().status()
+            }
         }
-    openshift.apply( dcConfig )
-    dc = openshift.selector( "dc/${appName}" )
-    dc.rollout().status()
     //sh """
     //   oc process -f ${repo}/deploymentconfig.yaml $parameters | oc apply -n $stage -f-
     // """
