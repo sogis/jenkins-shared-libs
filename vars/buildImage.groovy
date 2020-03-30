c7f526b70d360a4a9e87e1e25d8709fed8d00eca
pfeimich <mpfeiffer1975@gmail.com>
2020-03-30 12:29:10 +0200

    changed buildImage to scripted style because of generic build-args needed

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,16 +1,6 @@
-def call(String appName, String version, String repo, String build, List credentials, String qwcVersion) {
-    openshift.withCluster() {
-        openshift.withProject('gdi-devel') {
-            def buildConfig = openshift.process( "${repo}/buildconfig.yaml" )
-            openshift.apply( buildConfig )
-            withCredentials(credentials) {
-                if (qwcVersion != "") {
-                    def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg QWC2_VERSION=${qwcVersion}", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "--env GIT_PASSWORD=${gitlabPwd}" )
-                    }
-                else {
-                    def builds = openshift.selector( "bc", appName).startBuild( "--wait", "--build-arg GIT_VERSION=v${version}", "--env GIT_TOKEN=${gitlabToken}", "--env GIT_PASSWORD=${gitlabPwd}" )
-                    }
-                }  
-            }
-        }
+def call(String appName, String repo, String stage, String arguments) {
+    sh """
+       oc process -f $repo/buildconfig.yaml | oc apply -n $stage -f- 
+       oc start-build $appName -n $stage --wait $arguments
+    """
     }
