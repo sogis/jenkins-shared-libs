bde6e28a85d3bdc926a790dc949ddfde1dcb1564
pfeimich <mpfeiffer1975@gmail.com>
2020-04-01 11:20:23 +0200

    rollback last change

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 def call(String appName, String repo, String stage, String arguments) {
     sh """
-       oc process -n $stage -f $repo/buildconfig.yaml | oc apply -f- 
+       oc process -f $repo/buildconfig.yaml | oc apply -n $stage -f- 
        oc start-build $appName -n $stage --wait $arguments
     """
     }
