13445cf9e803e43a87e1864f40bbaf831727ddc0
pfeimich <mpfeiffer1975@gmail.com>
2020-04-01 11:12:26 +0200

    test with -n for oc process instead of oc apply

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 def call(String appName, String repo, String stage, String arguments) {
     sh """
-       oc process -f $repo/buildconfig.yaml | oc apply -n $stage -f- 
+       oc process -n $stage -f $repo/buildconfig.yaml | oc apply -f- 
        oc start-build $appName -n $stage --wait $arguments
     """
     }
