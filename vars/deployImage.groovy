76bcbb30535726df060e75811b641e5fa1d6757d
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-04-15 18:15:39 +0200

    deploymentConfig with ImageChange Trigger

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,9 +1,9 @@
 def call(String appName, String stage, String version, List dcConfig) {
     openshift.withCluster() {
         openshift.withProject(stage) {
-            for  ( o in dcConfig ) {
-                o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/${stage}/${appName}:${version}"
-                }
+            //for  ( o in dcConfig ) {
+            //    o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/${stage}/${appName}:${version}"
+            //    }
             openshift.apply( dcConfig )
             dc = openshift.selector( "dc/${appName}" )
             dc.rollout().status()
