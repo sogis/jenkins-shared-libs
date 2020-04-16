db3413cbaf0f3d1e559be615d46d0a1de1ce929d
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-04-16 15:37:20 +0200

    test -f flag in oc apply

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -4,7 +4,7 @@ def call(String appName, String stage, String version, List dcConfig) {
             for  ( o in dcConfig ) {
                 o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/${stage}/${appName}:${version}"
                 }
-            openshift.apply( dcConfig )
+            openshift.apply( dcConfig, "-f" )
             dc = openshift.selector( "dc/${appName}" )
             dc.rollout().latest()
             dc.rollout().status()
