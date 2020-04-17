934f38bfbd8b2df3fef315a9c92b0133bdd2ba61
pfeimich <mpfeiffer1975@gmail.com>
2020-04-17 15:20:13 +0200

    added varible repo to deployImage class

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,6 +1,6 @@
-def call(String appName, String stage, String version) {
+def call(String appName, String stage, String version, String repo) {
     sh """
-        oc process -f https://raw.githubusercontent.com/sogis/pipelines/master/api_webgisclient/data-service/deploymentconfig.yaml -p NAMESPACE=$stage -p TAG=$version | oc apply -n $stage -f-
+        oc process -f $repo/deploymentconfig.yaml -p NAMESPACE=$stage -p TAG=$version | oc apply -n $stage -f-
         oc rollout latest -n $stage $appName
         oc rollout status -n $stage dc $appName
     """
