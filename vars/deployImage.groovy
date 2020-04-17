1edc41e2d9127f771a7167484d62eaa4e2cecbfd
pfeimich <mpfeiffer1975@gmail.com>
2020-04-17 14:59:28 +0200

    cleanup code

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,17 +1,7 @@
-def call(String appName, String stage, String version, List dcConfig) {
-    openshift.withCluster() {
-        openshift.withProject(stage) {
-            for  ( o in dcConfig ) {
-                o.spec.template.spec.containers[0].image = "docker-registry.default.svc:5000/${stage}/${appName}:${version}"
-                }
-            sh """
-                oc process -f https://raw.githubusercontent.com/sogis/pipelines/master/api_webgisclient/data-service/deploymentconfig.yaml -p NAMESPACE=${stage} -p TAG=2.0.16 | oc apply -n $stage -f-
-                oc rollout latest -n $stage $appName
-                oc rollout status -n $stage dc $appName
-            """
-            //dc = openshift.selector( "dc/${appName}" )
-            //dc.rollout().latest()
-            //dc.rollout().status()
-            }
-        }
+def call(String appName, String stage, String version) {
+    sh """
+        oc process -f https://raw.githubusercontent.com/sogis/pipelines/master/api_webgisclient/data-service/deploymentconfig.yaml -p NAMESPACE=$stage -p TAG=$version | oc apply -n $stage -f-
+        oc rollout latest -n $stage $appName
+        oc rollout status -n $stage dc $appName
+    """
     }
