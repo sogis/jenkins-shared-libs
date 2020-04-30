e5218e3a5399d0f0a4674dce1c701b02f9d0aaca
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-04-30 17:08:18 +0200

    added argument params to deployImage class

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,6 +1,6 @@
-def call(String appName, String stage, String version, String repo) {
+def call(String appName, String stage, String version, String repo, String params = null) {
     sh """
-        oc process -f $repo/deploymentconfig.yaml -p NAMESPACE=$stage -p TAG=$version | oc apply -n $stage -f-
+        oc process -f $repo/deploymentconfig.yaml -p NAMESPACE=$stage -p TAG=$version $params | oc apply -n $stage -f-
         oc rollout latest -n $stage $appName
         oc rollout status -n $stage dc $appName
     """
