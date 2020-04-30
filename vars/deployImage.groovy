e29af38c87c21c210b2d95bad3fa51c481056672
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-04-30 17:22:23 +0200

    changed default value for params

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,4 +1,4 @@
-def call(String appName, String stage, String version, String repo, String params = null) {
+def call(String appName, String stage, String version, String repo, String params = "") {
     sh """
         oc process -f $repo/deploymentconfig.yaml -p NAMESPACE=$stage -p TAG=$version $params | oc apply -n $stage -f-
         oc rollout latest -n $stage $appName
