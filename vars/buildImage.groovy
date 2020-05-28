d2d7570eefec153e042ac501e14979f674699744
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-05-28 16:50:28 +0200

    added --from-repo to oc start-build

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 def call(String appName, String repo, String stage, String params, String arguments) {
     sh """
        oc process -f $repo/buildconfig.yaml $params | oc apply -n $stage -f- 
-       oc start-build $appName -n $stage --wait $arguments
+       oc start-build $appName -n $stage --from-repo=qwc_services --wait $arguments
     """
     }
