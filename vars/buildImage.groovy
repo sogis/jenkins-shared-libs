d40ebe5e3ad2088a3eae5cca5c890a20feab173b
pfeimich <mpfeiffer1975@gmail.com>
2020-06-16 16:41:49 +0200

    updated buildImage lib for use of config Files

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,21 @@
-def call(String appName, String repo, String stage, String params, String arguments) {
+def call(String appName, String repo, String stage, String params, String configFileName, String arguments) {
     sh """
+       if [ -d "qwc_services" ]; then
+         rm -rf qwc_services/* rm -rf qwc_services/.git
+       fi
+       git clone https://github.com/sogis/qwc_services.git
+    """
+    unstash 'configFile'
+    sh "cp config/default/* qwc_services/$appName"
+    dir("qwc_services") {
+    sh """
+       git config user.email mpfeiffer1975@gmail.com
+       git config user.name pfeimich
+       git add $appName/$configFileName.json
+       git commit -m 'added file'
+    """
+    }
+    sh """   
        oc process -f $repo/buildconfig.yaml $params | oc apply -n $stage -f- 
        oc start-build $appName -n $stage --from-repo=qwc_services --wait $arguments
     """
