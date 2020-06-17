ec39e065a4545f217413252aa6df2491219fd2ed
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-06-17 15:03:39 +0200

    removed stash command

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -5,5 +5,4 @@ def call() {
         chown www-data:www-data /tmp/workspace/data-service/config /tmp/workspace/data-service/legends
         PGSERVICEFILE=/var/www/.pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json service_configs
     """
-    stash allowEmpty: true, includes: 'config/**', name: 'configFile'
     }
