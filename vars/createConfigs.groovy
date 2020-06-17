ea0b0411fc4dca2b9c565753c4ede5f9116d9428
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-06-17 15:13:51 +0200

    added some debug info

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -3,6 +3,7 @@ def call() {
         mkdir /tmp/workspace/data-service/config
         mkdir /tmp/workspace/data-service/legends
         chown www-data:www-data /tmp/workspace/data-service/config /tmp/workspace/data-service/legends
+        ls -la
         PGSERVICEFILE=/var/www/.pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json service_configs
     """
     }
