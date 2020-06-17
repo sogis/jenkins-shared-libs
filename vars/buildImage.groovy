d4402eac7fe52c23f64ac1cbe8b486f2bdddacda
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-06-17 15:08:22 +0200

    changed debug info

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -12,7 +12,7 @@ def call(String appName, String repo, String stage, String params, String config
        git config user.email mpfeiffer1975@gmail.com
        git config user.name pfeimich
        git add $appName/$configFileName
-       ls -la
+       ls -la $appName
        git commit -m 'added file'
     """
     }
