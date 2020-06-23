44e5c0171a9d028328c0e7bb740d7dcf7f976e27
pfeimich <mpfeiffer1975@gmail.com>
2020-06-23 16:41:38 +0200

    changed to relative paths

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -1,8 +1,8 @@
 def call(Map params) {
     sh """
-        mkdir /tmp/workspace/data-service/config
-        mkdir /tmp/workspace/data-service/legends
-        chown www-data:www-data /tmp/workspace/data-service/config /tmp/workspace/data-service/legends
+        mkdir config
+        mkdir legends
+        chown www-data:www-data config legends
         PGSERVICEFILE=/var/www/.pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
     """
     }
