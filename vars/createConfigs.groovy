7eee6bda173ffc03b40320989527336342558b22
pfeimich <mpfeiffer1975@gmail.com>
2020-06-23 17:03:52 +0200

    changed paths for config and legend files

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -1,8 +1,8 @@
 def call(Map params) {
     sh """
-        mkdir config
-        mkdir legends
-        chown www-data:www-data config legends
-        PGSERVICEFILE=/var/www/.pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
+        mkdir /srv/qwc_service/config
+        mkdir /srv/qwc_service/legends
+        chown www-data:www-data /srv/qwc_service/config /srv/qwc_service/legends
+        PGSERVICEFILE=/var/www/.pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json service_configs
     """
     }
