9cc0f4b4ac5ae433319f2abdd29d723c0abdbca2
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-06-24 10:22:50 +0200

    added copying files to right directory and stash

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -4,5 +4,7 @@ def call(Map params) {
         mkdir /srv/qwc_service/legends
         chown www-data:www-data /srv/qwc_service/config /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/.pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json service_configs
+        cp -R /srv/qwc_service/config /srv/qwc_service/legends env.WORKSPACE
     """
+    stash allowEmpty: true, includes: 'config/**', name: 'configFile'
     }
