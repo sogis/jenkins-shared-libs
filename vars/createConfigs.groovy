9bf2890b8d2f5702d82784f8d558f33d0252da8f
pfeimich <mpfeiffer1975@gmail.com>
2020-07-07 10:28:40 +0200

    changed copy to move

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -4,7 +4,7 @@ def call(Map params) {
         mkdir /srv/qwc_service/legends
         chown www-data:www-data /srv/qwc_service/config /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/.pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
-        cp -R /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
+        mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     }
