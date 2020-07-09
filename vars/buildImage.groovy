5511ee365b88f7b886843fe091dbed8feea52a61
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-07-09 11:14:08 +0200

    replaced git add out of if condition

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -15,9 +15,11 @@ def call(String appName, String repo, String stage, String params, String config
        if ( repo == "gdi" ) {
           sh """
              sed -i "s|geo-i.so.ch/analytics|geo.so.ch/analytics|" $appName/index.html
-             git add $appName/index.html
           """
           }
+       sh """
+          git add $appName/index.html
+       """
        }
     sh """
        wget -r -np -nd -erobots=off -A '*.json' --reject-regex '/\\*.+\\*/|auto_refresh' --no-check-certificate --auth-no-challenge --user='$apiUser' --password='$PwdApiUser'  '$JENKINS_URL/job/$jobName/$buildNumber/artifact/config/default/' -P $appName
