430dd0e614a70a2406a395c1d6adef4a33d03987
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-07-23 12:27:54 +0200

    added stash

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -2,11 +2,13 @@ def call(Map params) {
     sh """
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
+        mkdir $env.WORKSPACE/requirements
         chown www-data:www-data /srv/qwc_service/config /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/.pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
-        cp /srv/qwc_service/requirementsImages.txt $env.WORKSPACE
+        cp /srv/qwc_service/requirementsImages.txt $env.WORKSPACE/requirements
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
     """
+    stash name: 'requirements', includes: 'requirements/**'
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
         rm -rf $env.WORKSPACE/legends $env.WORKSPACE/config
