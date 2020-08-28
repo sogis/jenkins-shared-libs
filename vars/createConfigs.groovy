02d3e9e20d46000a90a6f4e3fe9dd1537692836f
pfeimich <mpfeiffer1975@gmail.com>
2020-08-28 15:37:08 +0200

    removed ownership change

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -3,7 +3,6 @@ def call(Map params) {
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
         mkdir $env.WORKSPACE/requirements
-        chown www-data:www-data /srv/qwc_service/config /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/.pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
         cp /srv/qwc_service/requirementsImages.txt $env.WORKSPACE/requirements
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
