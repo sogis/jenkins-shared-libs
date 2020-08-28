13dfe82b2db0cdb4d7ce0c1aba23bdaf32e9590c
pfeimich <mpfeiffer1975@gmail.com>
2020-08-28 15:52:35 +0200

    removed deprecated requirements code

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -2,14 +2,8 @@ def call(Map params) {
     sh """
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
-        mkdir $env.WORKSPACE/requirements
         PGSERVICEFILE=/var/www/.pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
-        cp /srv/qwc_service/requirementsImages.txt $env.WORKSPACE/requirements
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
     """
-    stash name: 'requirements', includes: 'requirements/**'
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
-    sh """
-        rm -rf $env.WORKSPACE/legends $env.WORKSPACE/config $env.WORKSPACE/requirements
-    """
     }
