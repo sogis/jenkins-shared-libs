c81bd5058e19faf3ea490ae75d423c919f3bec44
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-09-04 12:41:19 +0200

    added generation of qgs to createConfigs

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -3,6 +3,7 @@ def call(Map params) {
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
+        PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json qgs
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
