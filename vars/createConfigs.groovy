d5af25ed986bc17b4501e7ec79ce41b5472cba5f
pfeimich <mpfeiffer1975@gmail.com>
2021-01-28 13:08:35 +0100

    fixed wrong path

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -5,8 +5,8 @@ def call(String appName, String namespace, String configFileName) {
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json qgs
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
-        oc create configmap $appName --from-file=$env.WORKSPACE/config/$appName/$configFileName -n $namespace
-        oc create configmap $appName --from-file=$env.WORKSPACE/config/permissions.json -n $namespace
+        oc create configmap $appName --from-file=$env.WORKSPACE/config/default/$configFileName -n $namespace
+        oc create configmap $appName --from-file=$env.WORKSPACE/config/default/permissions.json -n $namespace
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
