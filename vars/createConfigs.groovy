3e7370842f099cbf8d16effd1bb1fcb20b51db22
pfeimich <mpfeiffer1975@gmail.com>
2021-01-28 12:52:28 +0100

    test generating configMap

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -1,10 +1,12 @@
-def call(Map params) {
+def call(String appName, String namespace, String configFileName) {
     sh """
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json qgs
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
+        oc create configmap $appName --from-file=$env.WORKSPACE/config/$appName/$configFileName -n $namespace
+        oc create configmap $appName --from-file=$env.WORKSPACE/config/permissions.json -n $namespace
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
