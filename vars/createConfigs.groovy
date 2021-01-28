455afff07d26e11bab6281811c9980612fffa21a
pfeimich <mpfeiffer1975@gmail.com>
2021-01-28 17:02:54 +0100

    fixed typo

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -7,7 +7,7 @@ def call(String appName, String namespace, String configFileName) {
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json qgs
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
         oc delete configmap $configMapName
-        oc create configmap $configMapName --from-file=$env.WORKSPACE/config/default/$configFileName -n $namespace -l app=$sappName
+        oc create configmap $configMapName --from-file=$env.WORKSPACE/config/default/$configFileName -n $namespace -l app=$appName
         oc create configmap permission-json --from-file=$env.WORKSPACE/config/default/permissions.json -n $namespace
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
