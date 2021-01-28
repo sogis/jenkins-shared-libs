701dd69dac27ae023cb343d207058ddd465b499b
pfeimich <mpfeiffer1975@gmail.com>
2021-01-28 14:41:45 +0100

    change oc create to oc apply and fixed wrong configFile name

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -2,11 +2,12 @@ def call(String appName, String namespace, String configFileName) {
     sh """
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
+        configMapName = $(echo $configFileName | sed s/./-/g)
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json qgs
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
-        oc create configmap $appName --from-file=$env.WORKSPACE/config/default/$configFileName -n $namespace
-        oc create configmap $appName --from-file=$env.WORKSPACE/config/default/permissions.json -n $namespace
+        oc apply configmap $configMapName --from-file=$env.WORKSPACE/config/default/$configFileName -n $namespace
+        oc apply configmap permission-json --from-file=$env.WORKSPACE/config/default/permissions.json -n $namespace
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
