bbe743bade87b0cecbf541557d4632a8586df5ab
pfeimich <mpfeiffer1975@gmail.com>
2021-01-28 16:58:42 +0100

    test another syntax for shell script and test oc delete oc create

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -1,13 +1,14 @@
 def call(String appName, String namespace, String configFileName) {
-    configMapName = sh "echo \$configFileName | sed s/./-/g"
+    configMapName = sh "echo $configFileName | sed s/./-/g"
     sh """
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json qgs
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
-        oc apply configmap $configMapName --from-file=$env.WORKSPACE/config/default/$configFileName -n $namespace
-        oc apply configmap permission-json --from-file=$env.WORKSPACE/config/default/permissions.json -n $namespace
+        oc delete configmap $configMapName
+        oc create configmap $configMapName --from-file=$env.WORKSPACE/config/default/$configFileName -n $namespace -l app=$sappName
+        oc create configmap permission-json --from-file=$env.WORKSPACE/config/default/permissions.json -n $namespace
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
