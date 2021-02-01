231e56848ca1bd7ba9637c4db3552b95217f195c
pfeimich <mpfeiffer1975@gmail.com>
2021-02-01 14:39:01 +0100

    changed oc create command

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -1,14 +1,13 @@
 def call(String appName, String namespace, String configFileName) {
-    configMapName = sh """echo $configFileName | sed s/./-/g"""
+    sh "echo $configFileName"
+    configMapName = sh "echo $configFileName | sed s/./-/g"
     sh """
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json qgs
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
-        oc delete configmap $configMapName
-        oc create configmap $configMapName --from-file=$env.WORKSPACE/config/default/$configFileName -n $namespace -l app=$appName
-        oc create configmap permission-json --from-file=$env.WORKSPACE/config/default/permissions.json -n $namespace
+        oc create configmap $configMapName --from-file=$env.WORKSPACE/config/default/$configFileName --from-file=$env.WORKSPACE/config/default/permissions.json -n $namespace -l app=$appName --dry-run -o yaml | oc apply -f -
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
