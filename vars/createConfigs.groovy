e2d4fb585f5a59366877fbd4c0efeb2d9a27edab
pfeimich <mpfeiffer1975@gmail.com>
2021-02-04 15:26:43 +0100

    developed a new lib for generating configMaps

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -1,12 +1,10 @@
 def call(String appName, String namespace, String configFileName) {
-    configMapName = sh(returnStdout: true, script: "echo $configFileName | sed 's/\\./-/g' | tr '[:upper:]' '[:lower:]'").trim()
     sh """
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json qgs
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
-        oc create configmap $configMapName --from-file $env.WORKSPACE/config/default/$configFileName --from-file $env.WORKSPACE/config/default/permissions.json --dry-run -o yaml | oc replace --force -n $namespace -f -
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
