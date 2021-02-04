9fb7346aa3256d8171e6d51eb4eb77126aac746c
pfeimich <mpfeiffer1975@gmail.com>
2021-02-04 13:04:12 +0100

    removed old code and debug info.Changed oc apply to oc replace cause permissions.json is too long for oc apply

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -1,16 +1,12 @@
 def call(String appName, String namespace, String configFileName) {
-    //sh "configFileName=$configFileName"
     configMapName = sh(returnStdout: true, script: "echo $configFileName | sed 's/\\./-/g' | tr '[:upper:]' '[:lower:]'").trim()
-    sh "echo $configMapName"
-    //configMapNameTmp = sh(returnStdout: true, script: 'echo $configFileName | sed "s/\\./-/g"').trim()
-    //configMapName = sh(returnStdout: true, script: 'echo $configMapNameTmp | .trim()
     sh """
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json qgs
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
-        oc create configmap $configMapName --from-file $env.WORKSPACE/config/default/$configFileName --from-file $env.WORKSPACE/config/default/permissions.json --dry-run -o yaml | oc apply -n $namespace -f -
+        oc create configmap $configMapName --from-file $env.WORKSPACE/config/default/$configFileName --from-file $env.WORKSPACE/config/default/permissions.json --dry-run -o yaml | oc replace --force -n $namespace -f -
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
