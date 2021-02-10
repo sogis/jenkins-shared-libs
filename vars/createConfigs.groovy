1c1272b929dfa6ff766e6d27d71f34e30d9584b3
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2021-02-10 16:37:32 +0100

    move oc create for permissions configMap to createConfigs

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -1,9 +1,10 @@
-def call(Map params) {
+def call(String namespace) {
     sh """
         mkdir /srv/qwc_service/config
         mkdir /srv/qwc_service/legends
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json all
         PGSERVICEFILE=/var/www/pg_service.conf python3 /srv/qwc_service/config_generator.py /srv/qwc_service/configGeneratorConfig.json qgs
+        oc create configmap permission-json --from-file /srv/qwc_service/config/default/permissions.json --dry-run -o yaml | oc replace --force -n $namespace -f -
         mv /srv/qwc_service/config /srv/qwc_service/legends $env.WORKSPACE
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
