65e377442b1d590abbe499e0452cdcb89cdc3606
pfeimich <mpfeiffer1975@gmail.com>
2021-05-06 17:38:17 +0200

    added download for sql2json.jar

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -6,10 +6,12 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
         pwd
         ls -la
         git clone https://github.com/simi-so/sql2json.git
+        wget https://github.com/simi-so/sql2json/releases/download/1.1.20/sql2json-1.1.20.jar
         ls -la
-        chmod u+x sql2json/sql2json.jar
-        sql2json/sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t sql2json/testconfigs/dataConfig.json -o dataConfig.json
+        chmod u+x sql2json-1.1.20.jar
+        ./sql2json-1.1.20.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t sql2json/testconfigs/dataConfig.json -o dataConfig.json
         mkdir config
+        mv dataConfig.json config
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
