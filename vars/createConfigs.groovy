dd347c6c65fc63f775f8067f73a126f9f39a6c13
pfeimich <mpfeiffer1975@gmail.com>
2021-05-06 17:24:47 +0200

    [data-service] added some debug info

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -3,7 +3,10 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
         if [ -d "sql2json" ]; then
           rm -rf sql2json/* rm -rf sql2json/.git
         fi
+        pwd
+        ls -la
         git clone https://github.com/simi-so/sql2json.git
+        ls -la
         chmod u+x sql2json/sql2json.jar
         sql2json/sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t sql2json/testconfigs/dataConfig.json -o dataConfig.json
         mkdir config
