654b442520c6d3eea7ebc1e4ccfe7cd7b1cf8f7c
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2021-05-12 16:11:13 +0200

    changed pathes for templates to pipelines repo

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -5,13 +5,12 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
         fi
         pwd
         ls -la
-        git clone https://github.com/simi-so/sql2json.git
         wget https://github.com/simi-so/sql2json/releases/download/1.1.20/sql2json-1.1.20.jar
         ls -la
         chmod u+x sql2json-1.1.20.jar
-        java -jar sql2json-1.1.20.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t sql2json/testconfigs/${configFileName} -o ${configFileName} -s https://raw.githubusercontent.com/qwc-services/qwc-${serviceName}/master/schemas/qwc-${serviceName}.json
+        java -jar sql2json-1.1.20.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t api_webgisclient/sql2json/templates/${configFileName} -o ${configFileName} -s https://raw.githubusercontent.com/qwc-services/qwc-${serviceName}/master/schemas/qwc-${serviceName}.json
         mkdir config
-        mv ${configFileName} sql2json/testconfigs/permissions.json config
+        mv ${configFileName} api_webgisclient/sql2json/templates/permissions.json config
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
