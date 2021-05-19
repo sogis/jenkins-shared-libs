3c3d4488930afe05803500d009187deca3a9247b
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2021-05-19 18:47:11 +0200

    changed path to sql2json.jar

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -11,10 +11,10 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
         fi
         pwd
         ls -la
-        wget https://github.com/simi-so/sql2json/releases/download/1.1.20/sql2json-1.1.20.jar
+        wget https://github.com/simi-so/sql2json/releases/latest/download/sql2json.jar
         ls -la
-        chmod u+x sql2json-1.1.20.jar
-        java -jar sql2json-1.1.20.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t api_webgisclient/sql2json/templates/${serviceName}/${configFileName} -o ${configFileName} -s https://raw.githubusercontent.com/qwc-services/${schemaDir}/master/schemas/${mapping}-${serviceName}.json
+        chmod u+x sql2json.jar
+        java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t api_webgisclient/sql2json/templates/${serviceName}/${configFileName} -o ${configFileName} -s https://raw.githubusercontent.com/qwc-services/${schemaDir}/master/schemas/${mapping}-${serviceName}.json
         mkdir config
         mv ${configFileName} api_webgisclient/sql2json/templates/permissions.json config
     """
