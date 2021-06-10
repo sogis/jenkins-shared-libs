106874b195b7b4395858d0064bb00282dcba2d57
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2021-06-10 18:11:33 +0200

    changed path for created config files to mounted volume

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -9,14 +9,17 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
         if [ -d "sql2json" ]; then
           rm -rf sql2json/* rm -rf sql2json/.git
         fi
+        if [ -L "/qgs-resources/config/${serviceName}" ]; then
+          mkdir /qgs-resources/config/${serviceName}
+        fi
         pwd
         ls -la
         wget https://github.com/simi-so/sql2json/releases/latest/download/sql2json.jar
         ls -la
         chmod u+x sql2json.jar
-        java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t api_webgisclient/sql2json/templates/${serviceName}/${configFileName} -o ${configFileName} -s https://raw.githubusercontent.com/qwc-services/${schemaDir}/master/schemas/${mapping}-${serviceName}.json
-        mkdir config
-        mv ${configFileName} api_webgisclient/sql2json/templates/permissions.json config
+        java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t api_webgisclient/sql2json/templates/${serviceName}/${configFileName} -o /qgs-resources/config/${serviceName}/${configFileName} -s https://raw.githubusercontent.com/qwc-services/${schemaDir}/master/schemas/${mapping}-${serviceName}.json
+        //mkdir config
+        //mv ${configFileName} api_webgisclient/sql2json/templates/permissions.json config
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
