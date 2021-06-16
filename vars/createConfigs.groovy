5a88937655d06f5c3a9455193f29702e483becf2
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2021-06-16 09:57:10 +0200

    changed path to config files

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -10,8 +10,8 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
           rm -rf sql2json/* rm -rf sql2json/.git
         fi
 
-        if [ ! -d  "config/${serviceName}" ]; then
-          mkdir -p config/${serviceName}
+        if [ ! -d  "config/default" ]; then
+          mkdir -p config/default
         fi
 
         # get the sql2json.jar and set the necessary permissions
@@ -19,14 +19,14 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
         chmod u+x sql2json.jar
 
         # sql2json command to create the config file
-        java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t api_webgisclient/sql2json/templates/${serviceName}/${configFileName} -o config/${serviceName}/${configFileName} -s https://raw.githubusercontent.com/qwc-services/${schemaDir}/master/schemas/${mapping}-${serviceName}.json
+        java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t api_webgisclient/sql2json/templates/${serviceName}/${configFileName} -o config/default/${configFileName} -s https://raw.githubusercontent.com/qwc-services/${schemaDir}/master/schemas/${mapping}-${serviceName}.json
         
         # grep for qgis-server pod name
         ls -la
     """
         PODNAME= sh([script: 'oc get pods -o custom-columns=POD:.metadata.name --no-headers -n ${namespace} | grep qgis-server | grep -v -E -m 1 "featureinfo|build|print"', returnStdout: true]).trim()
     sh """
-        cp api_webgisclient/sql2json/templates/permissions.json config/${serviceName}
+        cp api_webgisclient/sql2json/templates/permissions.json config/default
         oc rsync -n ${namespace} config/ $PODNAME:/data/config
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
