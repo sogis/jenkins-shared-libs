7ac48b1ef626d8ac3408eb215ab525ebdfe57b16
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2021-07-16 12:20:32 +0200

    [createConfigs] Poc for WMS,WFS jsons

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -5,6 +5,14 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
     else {
         schemaDir = mapping + '-' + serviceName
         } 
+    if ( serviceName == "wms-qgs-content" || serviceName == "wfs-qgs-content" ) {
+        targetPath = '/data'
+        githubRepo = 'simi-so'
+        }
+    else {
+        targetPath = '/data/config'
+        githubRepo = 'qwc-services
+        }
     sh """ 
         if [ -d "sql2json" ]; then
           rm -rf sql2json/* rm -rf sql2json/.git
@@ -19,7 +27,7 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
         chmod u+x sql2json.jar
 
         # sql2json command to create the config file
-        java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t api_webgisclient/sql2json/templates/${serviceName}/template.json -o config/default/${configFileName} -s https://raw.githubusercontent.com/qwc-services/${schemaDir}/master/schemas/${mapping}-${serviceName}.json
+        java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t api_webgisclient/sql2json/templates/${serviceName}/template.json -o config/default/${configFileName} -s https://raw.githubusercontent.com/${githubRepo}/${schemaDir}/master/schemas/${mapping}-${serviceName}.json
         
         # grep for qgis-server pod name
         ls -la
@@ -27,7 +35,7 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
         PODNAME= sh([script: 'oc get pods -o custom-columns=POD:.metadata.name --no-headers -n ${namespace} | grep qgis-server | grep -v -E -m 1 "featureinfo|build|print"', returnStdout: true]).trim()
     sh """
         cp api_webgisclient/sql2json/templates/permissions.json config/default
-        oc rsync -n ${namespace} config/ $PODNAME:/data/config
+        oc rsync -n ${namespace} config/ $PODNAME:${targetPath}
     """
     archiveArtifacts artifacts: 'config/**', onlyIfSuccessful: true, allowEmptyArchive: true
     sh """
