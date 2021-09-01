de1c7fdb3adc37d3b3bb897acf73c763004310a1
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2021-09-01 15:15:30 +0200

    changed if condition for index.html

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -38,7 +38,7 @@ def call(String environment, String dbuser,String dbuserpwd, String dbserver, St
            cp -R api_webgisclient/landreg-service/grundbuchplanauszug.qgs api_webgisclient/landreg-service/print config
         """
         }
-    else if ( serviceName == "qwc-service" ) {
+    else if ( serviceName == "map-viewer" ) {
         if ( environment == "gdi-production" ) {
             sh """
                 sed -i "s|geo-i.so.ch/analytics|geo.so.ch/analytics|" api_webgisclient/qwc-service/index.html
