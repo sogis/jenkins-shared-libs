c9e4c75bd92f70d783c5f01133abb70c158ffb84
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2022-04-07 10:26:28 +0200

    remove dynamic branch from qwc-services repo

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -31,7 +31,7 @@ def call(String environment, String branch, String dbuser,String dbuserpwd, Stri
         fi
 
         # sql2json command to create the config file
-        java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t https://raw.githubusercontent.com/sogis/pipelines/${branch}/api_webgisclient/${serviceName}/sql2json/${templatePath} -o $env.WORKSPACE/config/default/${configFileName} -s https://raw.githubusercontent.com/${githubRepo}/${schemaDir}/${branch}/schemas/${mapping}-${schemaName}.json
+        java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t https://raw.githubusercontent.com/sogis/pipelines/${branch}/api_webgisclient/${serviceName}/sql2json/${templatePath} -o $env.WORKSPACE/config/default/${configFileName} -s https://raw.githubusercontent.com/${githubRepo}/${schemaDir}/master/schemas/${mapping}-${schemaName}.json
         
         # grep for qgis-server pod name
     """
