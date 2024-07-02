7714ff60c7da182551afc971665faaeaf6e9c9ab
pfeimich <michael.pfeiffer@bd.so.ch>
2024-07-02 19:08:00 +0200

    add second config file for qwc-service

--- vars/createConfigs.groovy
+++ vars/createConfigs.groovy
@@ -14,6 +14,12 @@ def call(String environment, String branch, String dbuser,String dbuserpwd, Stri
         targetPath = '/data/config'
         githubRepo = 'qwc-services'
         templatePath = 'template.json'
+        if ( serviceName == "qwc-service" ) {
+            mysochTemplatePath == 'mysoch_template.json'
+            }
+        else {
+            mysochTemplatePath == templatePath
+            }
         }
     sh """ 
         if [ -d "sql2json" ]; then
@@ -31,9 +37,12 @@ def call(String environment, String branch, String dbuser,String dbuserpwd, Stri
           chmod u+x sql2json.jar
         fi
 
-        # sql2json command to create the config file
+        # sql2json command to create the config file in default directory
         java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t https://raw.githubusercontent.com/sogis/pipelines/${branch}/api_webgisclient/${serviceName}/sql2json/${templatePath} -o $env.WORKSPACE/config/default/${configFileName} -s https://raw.githubusercontent.com/${githubRepo}/${schemaDir}/master/schemas/${mapping}-${schemaName}.json
         
+        # sql2json command to create the config file in mysoch directory
+        java -jar sql2json.jar -c jdbc:postgresql://${dbserver}:5432/${dbname} -u ${dbuser} -p ${dbuserpwd} -t https://raw.githubusercontent.com/sogis/pipelines/${branch}/api_webgisclient/${serviceName}/sql2json/${templatePath} -o $env.WORKSPACE/config/mysoch/${configFileName} -s https://raw.githubusercontent.com/${githubRepo}/${schemaDir}/master/schemas/${mapping}-${schemaName}.json
+
         # grep for qgis-server pod name
     """
     if ( schemaName == "wms-qgs-content" ) {
