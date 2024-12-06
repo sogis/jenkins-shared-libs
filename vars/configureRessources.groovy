b5abb7a54f8d4db06e0552c71c968e0ebac5b416
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-06 11:33:57 +0100

    remove wget and try with curl if param file exists

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,8 +1,6 @@
 def call(String appName, String stage, String repo, String environment){
     sh """
-       # Use wget to check if the URL exists
-       wget --spider -q "${repo}/${appName}_resources_${environment}.params"
-       if [ \$? -eq 0 ]; then
+       if [ curl -s -o /dev/null -w "%{http_code}" "${repo}/${appName}_resources_${environment}.params" == 200 ]; then
          oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
        else
          oc process -f ${repo}/resources.yaml | oc apply -n $stage -f
