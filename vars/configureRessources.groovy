d99b132b53293cce25bcb84ad0f8e1c1594867de
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-06 11:50:18 +0100

    try another comparison syntax

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -2,7 +2,7 @@ def call(String appName, String stage, String repo, String environment){
     sh """
        statuscode="curl -s -o /dev/null -w "%{http_code}" "${repo}/${appName}_resources_${environment}.params"
        echo $statuscode
-       if [ statuscode == 200 ]; then
+       if (( statuscode == 200 )); then
          oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
        else
          oc process -f ${repo}/resources.yaml | oc apply -n $stage -f-
