52ac773a680960b4cebac8928cec4483cd6b6487
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-06 11:06:28 +0100

    implement not exisiting params file

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,5 +1,8 @@
 def call(String appName, String stage, String repo, String environment){
     sh """
-       oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
+       if [ ! -f "${repo}/${appName}_resources_${environment}.params" ]; then
+         oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
+       else
+         oc process -f ${repo}/resources.yaml | oc apply -n $stage -f
     """
 }
