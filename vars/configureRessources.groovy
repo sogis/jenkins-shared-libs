29b72708957503deff0d6a47575e7c99320e1ba9
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-06 11:43:51 +0100

    fix wrong condition

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,6 +1,8 @@
 def call(String appName, String stage, String repo, String environment){
     sh """
-       if [ curl -s -o /dev/null -w "%{http_code}" "${repo}/${appName}_resources_${environment}.params" == 200 ]; then
+       statuscode="curl -s -o /dev/null -w "%{http_code}" "${repo}/${appName}_resources_${environment}.params"
+       echo $statuscode
+       if [ $statuscode == 200 ]; then
          oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
        else
          oc process -f ${repo}/resources.yaml | oc apply -n $stage -f-
