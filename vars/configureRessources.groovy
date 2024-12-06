59fcdc12744360b3f2bb7e9b37c7619777111cc2
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-06 11:26:41 +0100

    escape $

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,8 +1,8 @@
 def call(String appName, String stage, String repo, String environment){
     sh """
        # Use wget to check if the URL exists
-       wget --spider -q '${repo}/${appName}_resources_${environment}.params'
-       if [ $? -eq 0 ]; then
+       wget --spider -q "${repo}/${appName}_resources_${environment}.params"
+       if [ \$? -eq 0 ]; then
          oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
        else
          oc process -f ${repo}/resources.yaml | oc apply -n $stage -f
