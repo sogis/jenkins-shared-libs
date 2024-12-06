01cb202313d85081704d45604e37a88e5a041f23
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-06 11:35:03 +0100

    fix syntax error

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -3,7 +3,7 @@ def call(String appName, String stage, String repo, String environment){
        if [ curl -s -o /dev/null -w "%{http_code}" "${repo}/${appName}_resources_${environment}.params" == 200 ]; then
          oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
        else
-         oc process -f ${repo}/resources.yaml | oc apply -n $stage -f
+         oc process -f ${repo}/resources.yaml | oc apply -n $stage -f-
        fi
     """
 }
