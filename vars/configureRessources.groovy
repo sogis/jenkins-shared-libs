4844dce50712c8e1269a32a79389f1f1879abb05
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-06 11:18:05 +0100

    try single quotes

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,7 +1,7 @@
 def call(String appName, String stage, String repo, String environment){
     sh """
        # Use wget to check if the URL exists
-       wget --spider -q "${repo}/${appName}_resources_${environment}.params"
+       wget --spider -q '${repo}/${appName}_resources_${environment}.params'
        if [ $? -eq 0 ]; then
          oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
        else
