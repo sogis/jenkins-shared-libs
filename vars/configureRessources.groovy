b6f5720e9792418ee7fd4b3683a0791941fb8cd7
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-11 16:53:45 +0100

    add some debug info

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -2,6 +2,7 @@ def call(String appName, String stage, String repo, String environment){
     sh """
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
        if (( statuscode == 200 )); then
+         echo $DB_SERVER_TEST
          oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
        else
          oc process -f ${repo}/resources.yaml | oc apply -n $stage -f-
