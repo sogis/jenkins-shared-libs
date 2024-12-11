ad89d008f05bb75d7560167c6e28bff7a66cb0d0
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-11 12:38:57 +0100

    process param file with curl

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 def call(String appName, String stage, String repo, String environment, String arguments) {
     sh """
-       oc process -f ${repo}/buildconfig.yaml --param-file=${repo}/${appName}_build_${environment}.params | oc apply -n $stage -f- 
+       oc process -f ${repo}/buildconfig.yaml -p \$(curl -s ${repo}/${appName}_build_${environment}.params) | oc apply -n $stage -f- 
        oc start-build $appName -n $stage --from-repo=qwc_services --wait $arguments
     """
     }
