ca101be5677f1368909ae8e92dbae3e2a8a1b84c
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-11 12:23:51 +0100

    refactor buildImage class

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,6 @@
-def call(String appName, String repo, String stage, String params, String configFileName, String jobName, String buildNumber, String branch, String arguments) {
+def call(String appName, String stage, String repo, String environment, String arguments) {
     sh """
-       pwd
-       if [ -d "qwc_services" ]; then
-         rm -rf qwc_services/* rm -rf qwc_services/.git
-       fi
-       git clone -b $branch https://github.com/sogis/qwc_services.git
-       oc process -f $repo/buildconfig.yaml $params | oc apply -n $stage -f- 
+       oc process -f ${repo}/buildconfig.yaml --param-file={$repo}/${appName}_build_${environment}.params | oc apply -n $stage -f- 
        oc start-build $appName -n $stage --from-repo=qwc_services --wait $arguments
-       rm -rf qwc_services/* rm -rf qwc_services/.git
     """
     }
