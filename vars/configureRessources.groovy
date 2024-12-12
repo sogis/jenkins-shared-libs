acb1479f4954692efc575ee3afdc0b7cc8aab2a6
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-12 16:36:17 +0100

    uncommend row

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -4,7 +4,7 @@ def call(String appName, String stage, String repo, String environment){
        curl -L ${repo}/${appName}_resources_${environment}.params.gpg -o ${appName}_resources_${environment}.params.gpg
        if (( statuscode == 200 )); then
          pwd
-         gpg --quiet --batch --yes --decrypt --passphrase $env.GPG_DECRYPT_PW --output ${appName}_resources_${environment}.params ${appName}_resources_${environment}.params.gpg
+         # gpg --quiet --batch --yes --decrypt --passphrase $env.GPG_DECRYPT_PW --output ${appName}_resources_${environment}.params ${appName}_resources_${environment}.params.gpg
          oc process -f ${repo}/resources.yaml -p ${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
        else
          oc process -f ${repo}/resources.yaml | oc apply -n $stage -f-
