c266039a59eeb539d5b51b6f3c0f5218bcd6ca70
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-12 16:39:06 +0100

    uncomment another row

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,7 +1,7 @@
 def call(String appName, String stage, String repo, String environment){
     sh """
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
-       curl -L ${repo}/${appName}_resources_${environment}.params.gpg -o ${appName}_resources_${environment}.params.gpg
+       # curl -L ${repo}/${appName}_resources_${environment}.params.gpg -o ${appName}_resources_${environment}.params.gpg
        if (( statuscode == 200 )); then
          pwd
          # gpg --quiet --batch --yes --decrypt --passphrase $env.GPG_DECRYPT_PW --output ${appName}_resources_${environment}.params ${appName}_resources_${environment}.params.gpg
