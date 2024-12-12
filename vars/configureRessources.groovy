f6ae881449cf059f93113f761dcaae7b0ad7611d
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-12 15:44:08 +0100

    try to use gpg

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,9 +1,12 @@
 def call(String appName, String stage, String repo, String environment){
     sh """
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
+       curl -L ${repo}/${appName}_resources_${environment}.params.gpg -o ${appName}_resources_${environment}.params.gpg
        if (( statuscode == 200 )); then
-         echo $DB_SERVER_TEST
-         oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
+         pwd
+         echo "${env.GPG_DECRYPT_PW }" | gpg --batch --import
+         gpg --quiet --batch --yes --decrypt --output ${appName}_resources_${environment}.params ${appName}_resources_${environment}.params.gpg
+         oc process -f ${repo}/resources.yaml -p ${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
        else
          oc process -f ${repo}/resources.yaml | oc apply -n $stage -f-
        fi
