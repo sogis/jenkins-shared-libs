24c8cb42b300eab203f6a42212d714db6a852703
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-18 17:32:01 +0100

    remove debug info

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,7 +1,6 @@
 def call(String appName, String stage, String repo, String environment, String parameter){
     sh """
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
-       echo ${parameter}
        if (( statuscode == 200 )); then
          oc process -f ${repo}/resources.yaml -p ${parameter} | oc apply -n $stage -f- 
        else
