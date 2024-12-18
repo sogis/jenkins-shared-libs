749cc2abbf6798ff55b93c21de9c6ae39af6b215
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-18 17:50:40 +0100

    try some debug info

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,6 +1,7 @@
 def call(String appName, String stage, String repo, String environment, String parameter){
     sh """
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
+       echo "oc process -f ${repo}/resources.yaml -p ${parameter} | oc apply -n $stage -f-"
        if (( statuscode == 200 )); then
          oc process -f ${repo}/resources.yaml -p ${parameter} | oc apply -n $stage -f- 
        else
