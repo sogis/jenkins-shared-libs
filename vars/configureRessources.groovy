e765f20345f73183ae381a8dd61e21149e56b976
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-18 12:22:15 +0100

    fix typo

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -3,7 +3,7 @@ def call(String appName, String stage, String repo, String environment){
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
        params=\$(curl -s ${repo}/${appName}_resources_${environment}.params)
        echo "${params}"
-       echo "${DB_SERVER_TEST}
+       echo "${DB_SERVER_TEST}"
        if (( statuscode == 200 )); then
          oc process -f ${repo}/resources.yaml -p ${params} | oc apply -n $stage -f- 
        else
