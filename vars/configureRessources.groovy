c0db5270cd7c6bae06e50f3978f04dd58a32bc17
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-18 15:55:34 +0100

    remove debug infos and unnecessary param

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,9 +1,7 @@
 def call(String appName, String stage, String repo, String environment, String parameter){
     sh """
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
-       parameter=\$(curl -s ${repo}/${appName}_resources_${environment}.params)
        echo ${parameter}
-       echo ${DB_SERVER_TEST}
        if (( statuscode == 200 )); then
          oc process -f ${repo}/resources.yaml -p ${parameter} | oc apply -n $stage -f- 
        else
