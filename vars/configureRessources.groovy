07841edd297348804ba7ca651ff40354fea670c2
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-18 12:14:13 +0100

    revert back to commit b6f5720

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,11 +1,8 @@
 def call(String appName, String stage, String repo, String environment){
     sh """
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
-       curl -L ${repo}/${appName}_resources_${environment}.params.gpg -o ${appName}_resources_${environment}.params.gpg
        if (( statuscode == 200 )); then
-         pwd
-         gpg --quiet --batch --yes --decrypt --passphrase $env.GPG_DECRYPT_PW --output ${appName}_resources_${environment}.params ${appName}_resources_${environment}.params.gpg
-         oc process -f ${repo}/resources.yaml --param-file ${appName}_resources_${environment}.params | oc apply -n $stage -f- 
+         oc process -f ${repo}/resources.yaml -p \$(curl -s ${repo}/${appName}_resources_${environment}.params) | oc apply -n $stage -f- 
        else
          oc process -f ${repo}/resources.yaml | oc apply -n $stage -f-
        fi
