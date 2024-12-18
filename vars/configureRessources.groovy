7be7d7be1aa8dd6772a22728dd8917850a57f7ef
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-18 12:27:54 +0100

    change syntax

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -2,10 +2,10 @@ def call(String appName, String stage, String repo, String environment){
     sh """
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
        parameter=\$(curl -s ${repo}/${appName}_resources_${environment}.params)
-       echo "${parameter}"
+       echo ${parameter}
        echo "${DB_SERVER_TEST}"
        if (( statuscode == 200 )); then
-         oc process -f ${repo}/resources.yaml -p ${params} | oc apply -n $stage -f- 
+         oc process -f ${repo}/resources.yaml -p ${parameter} | oc apply -n $stage -f- 
        else
          oc process -f ${repo}/resources.yaml | oc apply -n $stage -f-
        fi
