d3deea0c33222caa68dc54928d760446df3de857
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-18 12:25:19 +0100

    change param name

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,8 +1,8 @@
 def call(String appName, String stage, String repo, String environment){
     sh """
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
-       params=\$(curl -s ${repo}/${appName}_resources_${environment}.params)
-       echo "${params}"
+       parameter=\$(curl -s ${repo}/${appName}_resources_${environment}.params)
+       echo "${parameter}"
        echo "${DB_SERVER_TEST}"
        if (( statuscode == 200 )); then
          oc process -f ${repo}/resources.yaml -p ${params} | oc apply -n $stage -f- 
