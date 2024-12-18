10545e97dfd6005af1939783837d5f4f3bb8e402
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-18 12:48:31 +0100

    test shift curl function in Jenkinsfile

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,11 +1,11 @@
-def call(String appName, String stage, String repo, String environment){
+def call(String appName, String stage, String repo, String environment, String parameter){
     sh """
        statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
        parameter=\$(curl -s ${repo}/${appName}_resources_${environment}.params)
-       echo \${parameter}
+       echo ${parameter}
        echo ${DB_SERVER_TEST}
        if (( statuscode == 200 )); then
-         oc process -f ${repo}/resources.yaml -p \${parameter} | oc apply -n $stage -f- 
+         oc process -f ${repo}/resources.yaml -p ${parameter} | oc apply -n $stage -f- 
        else
          oc process -f ${repo}/resources.yaml | oc apply -n $stage -f-
        fi
