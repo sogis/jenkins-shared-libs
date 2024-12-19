723d37f71282f9c10a34116e56546b09f3729ced
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-19 16:42:04 +0100

    switch to private repo

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,7 +4,8 @@ def call(String appName, String stage, String repo, String environment, String b
          rm -rf qwc_services/* rm -rf qwc_services/.git
        fi
        git clone -b $branch https://github.com/sogis/qwc_services.git
-       oc process -f ${repo}/buildconfig.yaml -p \$(curl -s ${repo}/${appName}_build_${environment}.params) | oc apply -n $stage -f- 
+       wget --header='Authorization: token ${PwdGitUser}' $repo/buildconfig.yaml
+       oc process -f buildconfig.yaml -p \$(curl -H 'Authorization: token ${PwdGitUser} -s ${repo}/${appName}_build_${environment}.params) | oc apply -n $stage -f- 
        oc start-build $appName -n $stage --from-repo=qwc_services --wait $arguments
        rm -rf qwc_services/* rm -rf qwc_services/.git
     """
