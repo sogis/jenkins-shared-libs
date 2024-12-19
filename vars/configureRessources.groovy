5b5ad571f24afbe544c9e6728f0164c27cf1829c
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-19 15:07:40 +0100

    try to use variable without handover

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,6 +1,6 @@
 def call(String appName, String stage, String repo, String environment, String parameter){
     sh """
-       statuscode=\$(curl -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
+       statuscode=\$(curl -H 'Authorization: token ${PwdGitUser}' -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
        if (( statuscode == 200 )); then
          oc process -f ${repo}/resources.yaml -p ${parameter} | oc apply -n $stage -f- 
        else
