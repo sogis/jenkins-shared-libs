12d41c3167da025739e0ef395a1520bdcd3fd1ea
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-23 12:36:05 +0100

    add accidently removed checkout of qwc_services repo

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,6 +4,7 @@ def call(String appName, String stage, String branch, String environment, String
          rm -rf qwc_services/* rm -rf qwc_services/.git
        fi
        git checkout ${branch}
+       git clone -b master https://github.com/sogis/qwc_services.git
        oc process -f api_webgisclient/${appName}/deployment/buildconfig.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_build_${environment}.params | oc apply -n ${stage} -f- 
        oc start-build ${appName} -n ${stage} --from-repo=qwc_services --wait ${arguments}
        rm -rf qwc_services/* rm -rf qwc_services/.git
