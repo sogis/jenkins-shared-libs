2911f873e84a65ef1a6bc25974662c12cb15a7fe
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-23 08:33:53 +0100

    switch to local files

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -4,10 +4,10 @@ def call(String appName, String stage, String repo, String environment, String b
        ls -la
        wget --header='Authorization: token ${PwdGitUser}' $repo/resources.yaml
        statuscode=\$(curl -H 'Authorization: token ${PwdGitUser}' -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
-       if (( statuscode == 200 )); then
-         oc process -f resources.yaml -p ${parameter} | oc apply -n $stage -f- 
+       if [ ! -f "api_webgisclient/${appName}/deployment/${appName}_resources_${environment}.params" ]; then
+         oc process -f api_webgisclient/${appName}/deployment/resources.yaml -p ${parameter} | oc apply -n $stage -f- 
        else
-         oc process -f resources.yaml | oc apply -n $stage -f-
+         oc process -f api_webgisclient/${appName}/deployment/resources.yaml | oc apply -n $stage -f-
        fi
     """
 }
