80e1a4b40b23d93210f47f9c29c084268c3f22f6
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-23 09:05:42 +0100

    remove bracket

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,7 +1,7 @@
 def call(String appName, String stage, String branch, String environment) {
     sh """
         git checkout ${branch}
-        oc process -f api_webgisclient/${appName}/deployment/deploymentconfig.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_deployment_${environment}.params) | oc apply -n $stage -f-
+        oc process -f api_webgisclient/${appName}/deployment/deploymentconfig.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_deployment_${environment}.params | oc apply -n $stage -f-
         oc rollout latest -n $stage $appName
         oc rollout status -n $stage dc $appName
         oc checkout ${branch_webgisclient}
