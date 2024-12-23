98b33c59ec1c39d33fe17d035649caa80b7936cb
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-23 12:10:37 +0100

    switch to local param file use

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,12 @@
-def call(String appName, String stage, String repo, String environment, String branch, String arguments) {
+def call(String appName, String stage, String branch, String environment, String arguments) {
     sh """
        if [ -d "qwc_services" ]; then
          rm -rf qwc_services/* rm -rf qwc_services/.git
        fi
-       git clone -b $branch https://github.com/sogis/qwc_services.git
-       wget --header='Authorization: token ${PwdGitUser}' $repo/buildconfig.yaml
-       oc process -f buildconfig.yaml -p \$(curl -H 'Authorization: token ${PwdGitUser} -s ${repo}/${appName}_build_${environment}.params) | oc apply -n $stage -f- 
-       oc start-build $appName -n $stage --from-repo=qwc_services --wait $arguments
+       git checkout ${branch}
+       oc process -f api_webgisclient/${appName}/deployment/buildconfig.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_build_${environment}.params) | oc apply -n ${stage} -f- 
+       oc start-build ${appName} -n ${stage} --from-repo=qwc_services --wait ${arguments}
        rm -rf qwc_services/* rm -rf qwc_services/.git
+       git checkout ${branch_webgisclient}
     """
     }
