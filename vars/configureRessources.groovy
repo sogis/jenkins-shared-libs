27d60e53d315f6dbd80547f7dd4eaaac9d3068b6
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-23 08:25:22 +0100

    checkout branch and added debug info

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,5 +1,7 @@
-def call(String appName, String stage, String repo, String environment, String parameter){
+def call(String appName, String stage, String repo, String environment, String branch, String parameter){
     sh """
+       git checkout ${branch}
+       ls -la
        wget --header='Authorization: token ${PwdGitUser}' $repo/resources.yaml
        statuscode=\$(curl -H 'Authorization: token ${PwdGitUser}' -s -o /dev/null -w %{http_code} ${repo}/${appName}_resources_${environment}.params)
        if (( statuscode == 200 )); then
