b3973b9578c79e886290f8e2ca34f3cd28e13de5
pfeimich <michael.pfeiffer@bd.so.ch>
2024-12-23 12:30:47 +0100

    remove bracket

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,7 +4,7 @@ def call(String appName, String stage, String branch, String environment, String
          rm -rf qwc_services/* rm -rf qwc_services/.git
        fi
        git checkout ${branch}
-       oc process -f api_webgisclient/${appName}/deployment/buildconfig.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_build_${environment}.params) | oc apply -n ${stage} -f- 
+       oc process -f api_webgisclient/${appName}/deployment/buildconfig.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_build_${environment}.params | oc apply -n ${stage} -f- 
        oc start-build ${appName} -n ${stage} --from-repo=qwc_services --wait ${arguments}
        rm -rf qwc_services/* rm -rf qwc_services/.git
        git checkout ${branch_webgisclient}
