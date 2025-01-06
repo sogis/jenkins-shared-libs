bc2f430f5c48b2bae50d967c113d7b02cab0c5f0
pfeimich <michael.pfeiffer@bd.so.ch>
2025-01-06 14:54:19 +0100

    remove git checkout and branch parameter

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,11 +1,10 @@
-def call(String appName, String stage, String branch, String environment, String parameter){
+def call(String appName, String stage, String environment, String parameter){
     sh """
-       git checkout ${branch}
        if [ ! -f "api_webgisclient/${appName}/deployment/${appName}_resources_${environment}.params" ]; then
          oc process -f api_webgisclient/${appName}/deployment/resources.yaml | oc apply -n $stage -f-
        else
          oc process -f api_webgisclient/${appName}/deployment/resources.yaml -p ${parameter} | oc apply -n $stage -f- 
        fi
-       git checkout master
+       git checkout ${branch_webgisclient}
     """
 }
