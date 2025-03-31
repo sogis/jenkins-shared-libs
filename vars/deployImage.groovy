277b4b9972057c09cda05e4483f236c1cb2218eb
pfeimich <michael.pfeiffer@bd.so.ch>
2025-03-31 17:44:30 +0200

    remove cpu limit in int and prod env

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,6 +1,11 @@
 def call(String appName, String stage, String branch, String environment) {
     sh """
         git checkout ${branch}
+    """
+    if (environment != 'test') {
+        sh "sed -i '/\${CPU_LIMIT}/d' api_webgisclient/${appName}/deployment/deploymentconfig.yaml"
+    }
+    sh """
         oc process -f api_webgisclient/${appName}/deployment/deploymentconfig.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_deployment_${environment}.params | oc apply -n $stage -f-
         oc rollout latest -n $stage $appName
         oc rollout status -n $stage dc $appName
