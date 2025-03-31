91ca958a8dc03c4f8ea4d162dbcd2b66b4ca236a
pfeimich <michael.pfeiffer@bd.so.ch>
2025-03-31 17:23:57 +0200

    remove cpu limit in int and prod env

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,4 +1,7 @@
 def call(String appName, String stage, String environment, String parameter){
+    if (environment != 'test') {
+        sh "sed -i '/\${CPU_LIMIT}/d' api_webgisclient/${appName}/deployment/resources.yaml"
+    }
     sh """
        if [ ! -f "api_webgisclient/${appName}/deployment/${appName}_resources_${environment}.params" ]; then
          oc process -f api_webgisclient/${appName}/deployment/resources.yaml | oc apply -n $stage -f-
