395b71503f753f1a04d231841c26530d4e4b9dfc
pfeimich <michael.pfeiffer@bd.so.ch>
2025-12-17 18:20:37 +0100

    change to deployment

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -6,7 +6,7 @@ def call(String appName, String stage, String branch, String environment) {
         sh "sed -i '/\${CPU_LIMIT}/d' api_webgisclient/${appName}/deployment/deploymentconfig.yaml"
     }
     sh """
-        oc process -f api_webgisclient/${appName}/deployment/deploymentconfig.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_deployment_${environment}.params | oc apply -n $stage -f-
+        oc process -f api_webgisclient/${appName}/deployment/deployment.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_deployment_${environment}.params | oc apply -n $stage -f-
         oc rollout latest -n $stage $appName
         oc rollout status -n $stage dc $appName
         git checkout ${branch_webgisclient}
