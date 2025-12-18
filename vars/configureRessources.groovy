27b993e9744e193fefaa674416fa8c77c5e0210a
pfeimich <michael.pfeiffer@bd.so.ch>
2025-12-18 14:58:32 +0100

    rollback to resources params file

--- vars/configureRessources.groovy
+++ vars/configureRessources.groovy
@@ -1,6 +1,6 @@
 def call(String appName, String stage, String environment, String parameter){
     sh """
-       if [ ! -f "api_webgisclient/${appName}/deployment/${appName}_deployment_${environment}.params" ]; then
+       if [ ! -f "api_webgisclient/${appName}/deployment/${appName}_resources_${environment}.params" ]; then
          oc process -f api_webgisclient/${appName}/deployment/resources.yaml | oc apply -n $stage -f-
        else
          oc process -f api_webgisclient/${appName}/deployment/resources.yaml -p ${parameter} | oc apply -n $stage -f- 
