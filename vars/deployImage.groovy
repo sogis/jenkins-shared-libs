141abf74b80f779272e25c483012f96102a80a83
pfeimich <michael.pfeiffer@bd.so.ch>
2026-01-07 15:58:31 +0100

    updated deployImage to remove CPU_LIMIT from deployment file for int and prod

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -3,7 +3,7 @@ def call(String appName, String stage, String branch, String environment) {
         git checkout ${branch}
     """
     if (environment != 'test') {
-        sh "sed -i '/\${CPU_LIMIT}/d' api_webgisclient/${appName}/deployment/deploymentconfig.yaml"
+        sh "sed -i '/\${CPU_LIMIT}/d; /{\\bCPU_LIMIT[A-Za-z0-9_]*\\b/d' api_webgisclient/${appName}/deployment/deployment.yaml"
     }
     sh """
         oc process -f api_webgisclient/${appName}/deployment/deployment.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_deployment_${environment}.params | oc apply -n $stage -f-
