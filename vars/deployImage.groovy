7ed5bc191db6f0a153157b246a0c7e3bdd82e315
pfeimich <michael.pfeiffer@bd.so.ch>
2026-01-08 15:12:50 +0100

    change rollout status to deployment

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -7,8 +7,7 @@ def call(String appName, String stage, String branch, String environment) {
     }
     sh """
         oc process -f api_webgisclient/${appName}/deployment/deployment.yaml --param-file api_webgisclient/${appName}/deployment/${appName}_deployment_${environment}.params | oc apply -n $stage -f-
-        oc rollout latest -n $stage $appName
-        oc rollout status -n $stage dc $appName
+        oc rollout status -n $stage deployment $appName
         git checkout ${branch_webgisclient}
     """
     }
